bac9ffc9cf5288c9a297c4122f0f6535fe549b55
kennylam91 <phamvanlam1991@gmail.com>
2019-08-05 16:59:39 +0700

    add basic Validator to class

--- src/app/register-form/register-form.component.ts
+++ src/app/register-form/register-form.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {FormsModule, FormControl, FormGroup} from '@angular/forms';
+import {Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-register-form',
@@ -9,13 +10,13 @@ import {FormsModule, FormControl, FormGroup} from '@angular/forms';
 export class RegisterFormComponent implements OnInit {
 
   register = new FormGroup({
-    email: new FormControl('',),
-    password: new FormControl(''),
-    confirmPassword: new FormControl(''),
-    country: new FormControl(''),
-    age: new FormControl(''),
-    gender: new FormControl(''),
-    phone: new FormControl('')
+    email: new FormControl('', Validators.required),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    confirmPassword: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    country: new FormControl('', Validators.required),
+    age: new FormControl('', [Validators.required, Validators.min(18)]),
+    gender: new FormControl('', Validators.required),
+    phone: new FormControl('', [Validators.required, Validators.pattern(/^\+84\d{9,10}$/)])
   });
 
   onSubmit() {
